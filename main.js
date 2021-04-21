583b39a64ab5eef0609230fa112aa039a7e84062
alexpintodiaz <alexanderp143@hotmail.com>
2021-04-21 18:32:51 -0500

    Taller_3.1

---
+++ main.js
@@ -0,0 +1,8 @@
+let A = parseInt(prompt("Ingrese por favor un número"))
+let B = parseInt(prompt("Ingrese por favor un número diferente"))
+
+if(A > B){
+    console.log(`El numero ${A} es el mayor`);
+}else{
+    console.log(`El numero ${B} es el mayor`);
+}
